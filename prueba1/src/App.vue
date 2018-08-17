c564be9598205ffa402ba6a4804386a53749071f
Jhoni Faber <jhonatanfaber@gmail.com>
2018-08-17 12:04:23 +0100

    fist vue example using props, emit

---
+++ prueba1/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p class="title">Hi {{ name }}</p>
+    <hr>
+    
+    <input v-model="userName">
+    <button @click="addUser">Add</button>
+    <hr>
+     <User
+            v-for="user in users" :key="user.id"
+            :sonName="user"
+            @buttonPressed="sayHello"
+    ></User>
+  </div>
+</template>
+
+<script>
+//v-model: two way databinding
+
+import User from "./components/User";
+
+export default {
+  name: "App",
+  components: {
+    User
+  },
+  data() {
+    return {
+      name: "Jhoni",
+      username: "",
+      users: []
+    };
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.userName);
+      this.userName = "";
+    },
+    sayHello(nameThatComesFromSon) {
+      this.name = nameThatComesFromSon;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ prueba1/src/components/User.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h3> {{sonName}} </h3>
+    <button @click="sayHello">Come on, say Hello</button>
+</div>
+    
+</template>
+
+<script>
+export default {
+  // props: Su valor es pasado desde el exterior.
+  props: ["sonName"],
+  methods: {
+    sayHello() {
+      //emit crea un evento personalizado el cual escucharemos
+      // con v-on en nuestro componente padre.
+      this.$emit("buttonPressed", this.sonName);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
