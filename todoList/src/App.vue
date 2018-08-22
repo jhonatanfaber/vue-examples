fbebe10a4b35a0d1674cce49301b0f3776a57c48
Jhoni Faber <jhonatanfaber@gmail.com>
2018-08-22 13:20:16 +0100

    todo list checkall option

---
+++ todoList/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <div class="list-wrapper">
+      <h1> Shopping List</h1>
+      <input v-model="product.name" @keyup.enter="addProduct">
+
+      <div class="list-wrapper__ buttons">
+        <button @click="addProduct">Add Product </button>
+        <button @click="checkAll">Check all </button>
+      
+      </div>
+
+      <div class="list-wrapper__product">
+        <product 
+          v-for="product in productList" :key="product.id"
+          :productName="product"
+      ></product>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Product from "./components/Product";
+
+export default {
+  name: "App",
+  components: {
+    Product
+  },
+  data() {
+    return {
+      product: {
+        name : "",
+        checked : false
+      },
+      productList: []
+    };
+  },
+  methods: {
+    addProduct() {
+      this.productList.push(this.product);
+      this.product = {
+        name: "",
+        checked: false
+      }
+    },
+    checkAll() {
+      this.productList.forEach(product => {
+        product.checked = !product.checked;
+      })
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.line {
+  color: blue;
+}
+</style>
---
+++ todoList/src/components/Product.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="productWrapper" :class="{'line':productName.checked}"> 
+            {{productName.name}} 
+        <input type="checkbox" v-model="productName.checked">
+    </div>
+</template>
+
+<script>
+export default {
+  //NOTE: :class="{'line': checked}" is only created when checked
+  //variable is true (see vue docu, Class and Style Bindings)
+  name: "Product",
+  props: ["productName"]
+};
+</script>
+
+<style >
+.line {
+  text-decoration: line-through;
+  color: rgb(110, 7, 7);
+}
+</style>
+
+
+
